32183793066f754d9b4d30b0c6432986a75db0a8
audition250601@gmail.com <audition250601@gmail.com>
2024-04-01 22:21:20 +0700

    final bug

---
+++ frontend/src/static/data.js
@@ -0,0 +1,384 @@
+// navigation Data
+export const navItems = [
+  {
+    title: "Trang chủ",
+    url: "/",
+  },
+  {
+    title: "Bán chạy",
+    url: "/best-selling",
+  },
+  {
+    title: "Sản phẩm",
+    url: "/products",
+  },
+  {
+    title: "Giảm giá",
+    url: "/events",
+  },
+  {
+    title: "Trợ giúp",
+    url: "/faq",
+  },
+];
+
+// branding data
+export const brandingData = [
+  {
+    id: 1,
+    title: "Miễn phí ship",
+    Description: "Cho hóa đơn từ 99k",
+    icon: (
+      <svg
+        width="36"
+        height="36"
+        viewBox="0 0 36 36"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1 1H5.63636V24.1818H35"
+          stroke="#FFBB38"
+          stroke-width="2"
+          stroke-miterlimit="10"
+          stroke-linecap="square"
+        ></path>
+        <path
+          d="M8.72763 35.0002C10.4347 35.0002 11.8185 33.6163 11.8185 31.9093C11.8185 30.2022 10.4347 28.8184 8.72763 28.8184C7.02057 28.8184 5.63672 30.2022 5.63672 31.9093C5.63672 33.6163 7.02057 35.0002 8.72763 35.0002Z"
+          stroke="#FFBB38"
+          stroke-width="2"
+          stroke-miterlimit="10"
+          stroke-linecap="square"
+        ></path>
+        <path
+          d="M31.9073 35.0002C33.6144 35.0002 34.9982 33.6163 34.9982 31.9093C34.9982 30.2022 33.6144 28.8184 31.9073 28.8184C30.2003 28.8184 28.8164 30.2022 28.8164 31.9093C28.8164 33.6163 30.2003 35.0002 31.9073 35.0002Z"
+          stroke="#FFBB38"
+          stroke-width="2"
+          stroke-miterlimit="10"
+          stroke-linecap="square"
+        ></path>
+        <path
+          d="M34.9982 1H11.8164V18H34.9982V1Z"
+          stroke="#FFBB38"
+          stroke-width="2"
+          stroke-miterlimit="10"
+          stroke-linecap="square"
+        ></path>
+        <path
+          d="M11.8164 7.18164H34.9982"
+          stroke="#FFBB38"
+          stroke-width="2"
+          stroke-miterlimit="10"
+          stroke-linecap="square"
+        ></path>
+      </svg>
+    ),
+  },
+  {
+    id: 2,
+    title: "Giảm giá hằng ngày",
+    Description: "Giảm đến 15%",
+    icon: (
+      <svg
+        width="32"
+        height="34"
+        viewBox="0 0 32 34"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M31 17.4502C31 25.7002 24.25 32.4502 16 32.4502C7.75 32.4502 1 25.7002 1 17.4502C1 9.2002 7.75 2.4502 16 2.4502C21.85 2.4502 26.95 5.7502 29.35 10.7002"
+          stroke="#FFBB38"
+          stroke-width="2"
+          stroke-miterlimit="10"
+        ></path>
+        <path
+          d="M30.7 2L29.5 10.85L20.5 9.65"
+          stroke="#FFBB38"
+          stroke-width="2"
+          stroke-miterlimit="10"
+          stroke-linecap="square"
+        ></path>
+      </svg>
+    ),
+  },
+  {
+    id: 4,
+    title: "Giá cả phải chăng",
+    Description: "Ở đâu rẻ hơn, ThanhThuong hoàn tiền",
+    icon: (
+      <svg
+        width="32"
+        height="35"
+        viewBox="0 0 32 35"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M7 13H5.5C2.95 13 1 11.05 1 8.5V1H7"
+          stroke="#FFBB38"
+          stroke-width="2"
+          stroke-miterlimit="10"
+        ></path>
+        <path
+          d="M25 13H26.5C29.05 13 31 11.05 31 8.5V1H25"
+          stroke="#FFBB38"
+          stroke-width="2"
+          stroke-miterlimit="10"
+        ></path>
+        <path
+          d="M16 28V22"
+          stroke="#FFBB38"
+          stroke-width="2"
+          stroke-miterlimit="10"
+        ></path>
+        <path
+          d="M16 22C11.05 22 7 17.95 7 13V1H25V13C25 17.95 20.95 22 16 22Z"
+          stroke="#FFBB38"
+          stroke-width="2"
+          stroke-miterlimit="10"
+          stroke-linecap="square"
+        ></path>
+        <path
+          d="M25 34H7C7 30.7 9.7 28 13 28H19C22.3 28 25 30.7 25 34Z"
+          stroke="#FFBB38"
+          stroke-width="2"
+          stroke-miterlimit="10"
+          stroke-linecap="square"
+        ></path>
+      </svg>
+    ),
+  },
+  {
+    id: 5,
+    title: "Thanh toán được bảo mật",
+    Description: "Đảm bảo an toàn 100%",
+    icon: (
+      <svg
+        width="32"
+        height="38"
+        viewBox="0 0 32 38"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M22.6654 18.667H9.33203V27.0003H22.6654V18.667Z"
+          stroke="#FFBB38"
+          stroke-width="2"
+          stroke-miterlimit="10"
+          stroke-linecap="square"
+        ></path>
+        <path
+          d="M12.668 18.6663V13.6663C12.668 11.833 14.168 10.333 16.0013 10.333C17.8346 10.333 19.3346 11.833 19.3346 13.6663V18.6663"
+          stroke="#FFBB38"
+          stroke-width="2"
+          stroke-miterlimit="10"
+          stroke-linecap="square"
+        ></path>
+        <path
+          d="M31 22C31 30.3333 24.3333 37 16 37C7.66667 37 1 30.3333 1 22V5.33333L16 2L31 5.33333V22Z"
+          stroke="#FFBB38"
+          stroke-width="2"
+          stroke-miterlimit="10"
+          stroke-linecap="square"
+        ></path>
+      </svg>
+    ),
+  },
+];
+
+// categories data
+export const categoriesData = [
+  {
+    id: 1,
+    title: "Thực phẩm chức năng",
+    subTitle: "",
+    image_Url:
+      "https://suckhoedoisong.qltns.mediacdn.vn/Images/quangcao/2018/01/25/suckhoedoisong.vn-_Tin_thng-_Thng_o.jpg",
+  },
+  {
+    id: 2,
+    title: "Dược mỹ phẩm",
+    subTitle: "",
+    image_Url:
+      "https://thanhnien.mediacdn.vn/Uploaded/dieutrang-qc/2021_10_22/mai-han-duoc-my-pham-2-4439.png",
+  },
+  {
+    id: 3,
+    title: "Chăm sóc cá nhân",
+    subTitle: "",
+    image_Url:
+      "https://s-imgcdn.tapchicongthuong.vn/tcct-media/23/1/21/cham-soc-ca-nhan-thien-nhien.jpg",
+  },
+  {
+    id: 4,
+    title: "Chăm sóc sức khỏe",
+    subTitle: "",
+    image_Url:
+      "https://timo.vn/wp-content/uploads/mua-bao-hiem-cham-soc-suc-khoe-toan-cau.png",
+  },
+  {
+    id: 5,
+    title: "Thuốc",
+    subTitle: "",
+    image_Url:
+      "https://cdnmedia.baotintuc.vn/Upload/r2ZmuVn2vsFEWUzMUAXAg/files/2023/11/Thu-tuc-tieu-huy-thuoc-tan-duoc-da-het-han-su-dung-1.jpg",
+  },
+  {
+    id: 6,
+    title: "Quà tặng",
+    subTitle: "",
+    image_Url:
+      "https://hips.hearstapps.com/hmg-prod/images/close-up-of-stack-gifts-on-table-against-blue-royalty-free-image-1676327737.jpg?crop=0.668xw:1.00xh;0,0&resize=640:*",
+  },
+  {
+    id: 7,
+    title: "Mẹ và bé",
+    subTitle: "",
+    image_Url: "https://bota.vn/wp-content/uploads/2018/11/bg3.jpg",
+  },
+  {
+    id: 8,
+    title: "Hỗ trợ làm đẹp",
+    subTitle: "",
+    image_Url:
+      "https://product.hstatic.net/200000736563/product/upload_0ca81b253d09412f901c19f2d5928a52_master.jpg",
+  },
+  {
+    id: 9,
+    title: "Thiết bị y tế",
+    subTitle: "",
+    image_Url:
+      "https://nhapkhautrungquoc.vn/wp-content/uploads/2021/07/Trang-thiet-bi-y-te-nhap-khau-Trung-Quoc.jpg",
+  },
+  {
+    id: 10,
+    title: "Khuyến mãi hot🔥",
+    subTitle: "",
+    image_Url:
+      "https://openend.vn/wp-content/uploads/2022/01/chuong-trinh-khuyen-mai-trong-kinh-doanh-scaled.jpg",
+  },
+];
+
+// product Data
+export const productData = [
+  {
+    id: 1,
+    category: "Thực phẩm chức năng",
+    name: "Thực phẩm chức năng Vi Sinh Biotic",
+    specifications: "Hộp 40 cái",
+    description:
+      "Product details are a crucial part of any eCommerce website or online marketplace. These details help the potential customers to make an informed decision about the product they are interested in buying. A well-written product description can also be a powerful marketing tool that can help to increase sales.Product details typically include information about the product's features, specifications, dimensions, weight, materials, and other relevant information that can help customers to understand the product better. The product details section should also include high-quality images and videos of the product, as well as customer reviews and ratings.",
+    image_Url: [
+      {
+        public_id: "test",
+        url: "https://www.hangngoainhap.com.vn/images/202210/goods_img/409-p2-1666689521.jpg",
+      },
+
+      {
+        public_id: "test",
+        url: "https://www.hangngoainhap.com.vn/images/202210/goods_img/409-p2-1666689521.jpg",
+      },
+    ],
+    shop: {
+      name: "Thanh Thuong.",
+      shop_avatar: {
+        public_id: "test",
+        url: "https://www.hangngoainhap.com.vn/images/202210/goods_img/409-p2-1666689521.jpg",
+      },
+      ratings: 4.2,
+    },
+    price: 836000,
+    discount_price: 836000,
+    rating: 4,
+    total_sell: 35,
+    stock: 10,
+  },
+  {
+    id: 2,
+    category: "Thực phẩm chức năng",
+    name: "Thực phẩm chức năng Biotic Vi sinh",
+    specifications: "Hộp 40 cái",
+    description:
+      "Men vi sinh BioGaia Protectis Baby Drops bổ sung vi khuẩn có lợi cho hệ đường ruột phát triển khỏe mạnh, tăng cường miễn dịch, kích thích trẻ ăn ngon, tiêu hóa tốt.",
+    image_Url: [
+      {
+        public_id: "test",
+        url: "https://www.hangngoainhap.com.vn/images/202210/goods_img/409-p2-1666689521.jpg",
+      },
+      {
+        public_id: "test",
+        url: "https://www.hangngoainhap.com.vn/images/202210/goods_img/409-p2-1666689521.jpg",
+      },
+    ],
+    shop: {
+      name: "Thanh Thương",
+      shop_avatar: {
+        public_id: "test",
+        url: "https://www.hangngoainhap.com.vn/images/202210/goods_img/409-p2-1666689521.jpg",
+      },
+      ratings: 4.2,
+    },
+    discount_price: 836000,
+    rating: 5,
+    total_sell: 80,
+    stock: 10,
+    category: "Thực phẩm chức năng",
+  },
+];
+
+export const footerProductLinks = [
+  {
+    name: "Về chúng tôi",
+    link: "/about",
+  },
+  {
+    name: "Careers",
+    link: "/carrers",
+  },
+  {
+    name: "Địa chỉ cửa hàng",
+  },
+  {
+    name: "Trang xã hội",
+  },
+  {
+    name: "Đánh giá",
+  },
+];
+
+export const footercompanyLinks = [
+  {
+    name: "Thực phẩm chức năng",
+  },
+  {
+    name: "Dược mỹ phẩm",
+  },
+  {
+    name: "Chăm sóc cá nhân",
+  },
+  {
+    name: "Mẹ và bé",
+  },
+  {
+    name: "Sự kiện",
+  },
+];
+
+export const footerSupportLinks = [
+  {
+    name: "Trợ giúp",
+  },
+  {
+    name: "Đánh giá",
+  },
+  {
+    name: "Liên hệ",
+  },
+  {
+    name: "Shipping",
+  },
+  {
+    name: "Chat trực tuyến",
+  },
+];
