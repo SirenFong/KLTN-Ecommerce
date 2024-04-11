215872555c714921e82b7ffdeddfb29d26235f78
SirenFong <audition250601@gmail.com>
2024-04-11 16:16:36 +0700

    Refactor UI and add load more functionality in EventsPage.jsx

--- frontend/src/pages/EventsPage.jsx
+++ frontend/src/pages/EventsPage.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import EventCard from "../components/Events/EventCard";
 import Header from "../components/Layout/Header";
 import Loader from "../components/Layout/Loader";
+import { BsArrowRight } from "react-icons/bs"; // Import the icon you want to use
 
 const EventsPage = () => {
   const { allEvents, isLoading } = useSelector((state) => state.events);
+  const [visibleEvents, setVisibleEvents] = useState(3); // State to control the number of visible events
+
+  const showMoreEvents = () => {
+    setVisibleEvents(visibleEvents + 3); // Show 3 more events when the button is clicked
+  };
+
   return (
     <>
       {isLoading ? (
@@ -13,7 +20,24 @@ const EventsPage = () => {
       ) : (
         <div>
           <Header activeHeading={4} />
-          <EventCard active={true} data={allEvents && allEvents[0]} />
+
+          {allEvents &&
+            allEvents.slice(0, visibleEvents).map((event, index) => (
+              <div
+                key={index}
+                style={{ border: "1px solid black", margin: "10px" }}
+              >
+                <EventCard active={true} data={event} />
+              </div>
+            ))}
+          {allEvents && allEvents.length > visibleEvents && (
+            <button
+              onClick={showMoreEvents}
+              style={{ display: "flex", alignItems: "center" }}
+            >
+              Xem thêm <BsArrowRight />
+            </button>
+          )}
         </div>
       )}
     </>
