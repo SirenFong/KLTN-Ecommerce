a9021fee50f50e5157840407d16080a8adcefef6
SirenFong <audition250601@gmail.com>
2024-05-08 20:47:25 +0700

    deployment

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,3 +1,3 @@
-export const server = "http://localhost:8000/api/v2";
+export const server = "https://ql-nhathuoc.vercel.app/api/v2";
 
-export const backend_url = "http://localhost:8000/";
+export const backend_url = "https://ql-nhathuoc.vercel.app/";
