5ad3acf024eba98fa3f9d1de19e326698d801875
SirenFong <audition250601@gmail.com>
2024-05-21 13:02:15 +0700

    fix issue

--- backend/model/user.js
+++ backend/model/user.js
@@ -56,6 +56,11 @@ const userSchema = new mongoose.Schema({
       },
     },
   ],
+  wishList: [
+    {
+      product: productSchema,
+    },
+  ],
   avatar: {
     public_id: {
       type: String,
